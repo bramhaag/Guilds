3a346c8d0bd2ebb08d0fbcf4e7525f38fc185686
Bram Hagens <bramhagens@hotmail.nl>
2016-12-31 10:19:16 +0100

    MySQL things

--- src/main/java/me/bramhaag/guilds/database/databases/mysql/MySql.java
+++ src/main/java/me/bramhaag/guilds/database/databases/mysql/MySql.java
@@ -1,14 +1,25 @@
 package me.bramhaag.guilds.database.databases.mysql;
 
+import co.aikar.taskchain.TaskChain;
+import co.aikar.taskchain.TaskChainFactory;
+import com.sun.rowset.CachedRowSetImpl;
 import com.zaxxer.hikari.HikariDataSource;
 import me.bramhaag.guilds.Main;
 import me.bramhaag.guilds.database.Callback;
 import me.bramhaag.guilds.database.DatabaseProvider;
 
 import me.bramhaag.guilds.guild.Guild;
+import me.bramhaag.guilds.guild.GuildMember;
+import me.bramhaag.guilds.guild.GuildRole;
 import org.bukkit.configuration.ConfigurationSection;
 
+import javax.sql.rowset.CachedRowSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.logging.Level;
 
 
 public class MySql extends DatabaseProvider {
@@ -36,19 +47,61 @@ public class MySql extends DatabaseProvider {
         hikari.validate();
 
         Main.newChain()
-            .async(() -> updateQuery(Query.CREATE_TABLE_GUILDS))
-            .async(() -> updateQuery(Query.CREATE_TABLE_MEMBERS))
-            .execute();
+            .async(() -> execute(Query.CREATE_TABLE_GUILDS))
+            .async(() -> execute(Query.CREATE_TABLE_MEMBERS))
+            .async(() -> execute(Query.CREATE_TABLE_INVITED_MEMBERS))
+            .sync(() -> Main.getInstance().getLogger().log(Level.INFO, "Tables 'guilds', 'members' and 'invited_members' created!"))
+        .execute();
     }
 
     @Override
     public void createGuild(Guild guild, Callback<Boolean, Exception> callback) {
+        TaskChain<?> chain = Main.newChain();
 
+        chain
+            .async(() -> {
+                boolean result = execute(Query.CREATE_GUILD, guild.getName());
+                chain.setTaskData("create_guild", result);
+            })
+            .async(() -> {
+                boolean result = execute(Query.ADD_MEMBER, guild.getGuildMaster().getUniqueId().toString(), guild.getName(), GuildRole.MASTER.getLevel());
+                chain.setTaskData("add_member", result);
+            })
+            .sync(() -> {
+                boolean createGuildResult = chain.getTaskData("create_guild");
+                boolean addMemberResult = chain.getTaskData("add_member");
+
+                callback.call(createGuildResult && addMemberResult, null);
+            })
+        .execute();
     }
 
     @Override
     public void removeGuild(Guild guild, Callback<Boolean, Exception> callback) {
+        TaskChain<?> chain = Main.newChain();
 
+        chain.
+            async(() -> {
+                boolean result = true;
+
+                for(GuildMember member : guild.getMembers()) {
+                    if(!execute(Query.REMOVE_MEMBER, member.getUniqueId())) {
+                        result = false;
+                    }
+                }
+
+                chain.setTaskData("remove_member", result);
+            })
+            .async(() -> {
+                boolean result = execute(Query.REMOVE_GUILD, guild.getName());
+                chain.setTaskData("remove_guild", result);
+            })
+            .sync(() -> {
+                boolean removeMemberResult = chain.getTaskData("remove_member");
+                boolean removeGuildResult = chain.getTaskData("remove_guild");
+
+                callback.call(removeMemberResult && removeGuildResult, null);
+            });
     }
 
     @Override
@@ -61,7 +114,51 @@ public class MySql extends DatabaseProvider {
 
     }
 
-    private void updateQuery(String query) {
-        return;
+    private boolean execute(String query, Object... parameters) {
+
+        Connection connection;
+        PreparedStatement statement;
+
+        try {
+            connection = hikari.getConnection();
+            statement = connection.prepareStatement(query);
+
+            for (int i = 0; i < parameters.length; i++) {
+                statement.setObject(i + 1, parameters[i]);
+            }
+
+            statement.execute();
+            return true;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private ResultSet executeQuery(String query, Object... parameters) {
+        Connection connection;
+        PreparedStatement statement;
+
+        try {
+            connection = hikari.getConnection();
+            statement = connection.prepareStatement(query);
+
+            for (int i = 0; i < parameters.length; i++) {
+                statement.setObject(i + 1, parameters[i]);
+            }
+
+            CachedRowSet resultCached = new CachedRowSetImpl();
+            ResultSet resultSet = statement.executeQuery();
+
+            resultCached.populate(resultSet);
+            resultSet.close();
+
+            return resultCached;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/main/java/me/bramhaag/guilds/database/databases/mysql/Query.java
+++ src/main/java/me/bramhaag/guilds/database/databases/mysql/Query.java
@@ -2,14 +2,26 @@ package me.bramhaag.guilds.database.databases.mysql;
 
 public class Query {
 
-    public static final String CREATE_TABLE_GUILDS = "CREATE IF NOT EXISTS guilds (" +
-                                                     "name VARCHAR(255) NOT NULL, " +
-                                                     "PRIMARY KEY (name)";
-    public static final String CREATE_TABLE_MEMBERS = "CREATE IF NOT EXISTS guild_members ( " +
-                                                      "uuid VARCHAR(36) NOT NULL, " +
-                                                      "role INTEGER NOT NULL, " +
-                                                      "PRIMARY KEY (uuid)";
-    public static final String CREATE_GUILD = "";
-    public static final String REMOVE_GUILD = "";
-    public static final String GET_GUILD = "";
+    public static final String CREATE_TABLE_GUILDS =          "CREATE IF NOT EXISTS guilds (" +
+                                                              "name VARCHAR(255) NOT NULL, " +
+                                                              "PRIMARY KEY (name)";
+    public static final String CREATE_TABLE_MEMBERS =         "CREATE IF NOT EXISTS guild_members ( " +
+                                                              "uuid VARCHAR(36) NOT NULL, " +
+                                                              "guild VARCHAR(255) NOT NULL" +
+                                                              "role INTEGER NOT NULL, " +
+                                                              "PRIMARY KEY (uuid)";
+    public static final String CREATE_TABLE_INVITED_MEMBERS = "CREATE IF NOT EXISTS invited_members ( " +
+                                                              "uuid VARCHAR(36) NOT NULL, " +
+                                                              "guild VARCHAR(255) NOT NULL";
+
+    public static final String CREATE_GUILD =                 "INSERT INTO guilds (name) VALUES(?)";
+    public static final String REMOVE_GUILD =                 "DELETE FROM guilds WHERE name=?";
+
+    public static final String ADD_MEMBER =                   "INSERT INTO guild_members (uuid, guild, role) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE guild=VALUES(guild), role=VALUES(role)";
+    public static final String REMOVE_MEMBER =                "DELETE FROM guild_members WHERE uuid=?";
+
+    public static final String ADD_INVITED_MEMBER =           "INSERT INTO invited_members (uuid, guild) VALUES(?, ?)";
+    public static final String REMOVE_INVITED_MEMBER =        "DELETE FROM invited_members WHERE uuid=?";
+
+    public static final String GET_GUILD_MEMBERS =            "SELECT uuid, role FROM guild_members WHERE guild=?";
 }
