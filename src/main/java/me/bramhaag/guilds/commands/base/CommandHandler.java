d15bd419aefabcb6d68a17651609021afe884fc3
Bram Hagens <bramhagens@hotmail.nl>
2017-01-08 14:32:16 +0100

    Added tab completion

--- src/main/java/me/bramhaag/guilds/commands/base/CommandHandler.java
+++ src/main/java/me/bramhaag/guilds/commands/base/CommandHandler.java
@@ -6,14 +6,13 @@ import me.bramhaag.guilds.util.ConfirmAction;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class CommandHandler implements CommandExecutor, IHandler {
+public class CommandHandler implements CommandExecutor, TabCompleter, IHandler {
 
     private List<CommandBase> commands;
     private HashMap<CommandSender, ConfirmAction> actions;
@@ -105,5 +104,32 @@ public class CommandHandler implements CommandExecutor, IHandler {
 
     public void removeAction(CommandSender sender) {
         actions.remove(sender);
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("guild")) {
+            if(args.length == 1) {
+                List<String> commandNames = new ArrayList<>();
+
+                if(!args[0].equals("")) {
+                    for(String commandName : commands.stream().map(CommandBase::getName).collect(Collectors.toList())) {
+                        if(!commandName.startsWith(args[0].toLowerCase())) {
+                            continue;
+                        }
+
+                        commandNames.add(commandName);
+                    }
+                }
+                else {
+                    commandNames = commands.stream().map(CommandBase::getName).collect(Collectors.toList());
+                }
+
+                Collections.sort(commandNames);
+                return commandNames;
+            }
+        }
+
+        return null;
     }
 }
