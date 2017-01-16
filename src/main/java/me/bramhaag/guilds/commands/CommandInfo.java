bd68b863a0e20c2e508559e2cde0a5f54a067280
Bram Hagens <bramhagens@hotmail.nl>
2017-01-16 19:45:59 +0100

    Fixed #18 and #19

--- src/main/java/me/bramhaag/guilds/commands/CommandInfo.java
+++ src/main/java/me/bramhaag/guilds/commands/CommandInfo.java
@@ -5,7 +5,10 @@ import me.bramhaag.guilds.guild.Guild;
 import me.bramhaag.guilds.guild.GuildRole;
 import me.bramhaag.guilds.message.Message;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+
+import java.util.stream.Collectors;
 
 public class CommandInfo extends CommandBase {
 
@@ -25,7 +28,7 @@ public class CommandInfo extends CommandBase {
         Message.sendMessage(player, Message.COMMAND_INFO_HEADER.replace("{guild}", guild.getName()));
         Message.sendMessage(player, Message.COMMAND_INFO_NAME.replace("{guild}", guild.getName(), "{prefix}", guild.getPrefix()));
         Message.sendMessage(player, Message.COMMAND_INFO_MASTER.replace("{master}", Bukkit.getPlayer(guild.getGuildMaster().getUniqueId()).getName()));
-        Message.sendMessage(player, Message.COMMAND_INFO_MEMBER_COUNT.replace("{members}", String.valueOf(guild.getMembers().size())));
+        Message.sendMessage(player, Message.COMMAND_INFO_MEMBER_COUNT.replace("{members}", String.valueOf(guild.getMembers().size()), "{members-online}", String.valueOf(guild.getMembers().stream().map((member) -> Bukkit.getPlayer(member.getUniqueId())).collect(Collectors.toList()).stream().filter(OfflinePlayer::isOnline).collect(Collectors.toList()).size())));
         Message.sendMessage(player, Message.COMMAND_INFO_RANK.replace("{rank}", GuildRole.getRole(guild.getMember(player.getUniqueId()).getRole()).getName()));
     }
 }
